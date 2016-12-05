020b0431ed17459862bf24d5efcaaafa34e8ffd3
Allen Tain <tain.allen@gmail.com>
2016-12-05 15:34:29 -0600

    got forms to work

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,9 +3,9 @@ class StatsController < ApplicationController
   require 'httparty'
   def index
     api = UrlApi.new()
-    @region ||= params[:region].nil? ? "us" : params[:region]
-    @league ||= params[:region].nil? ? 6 : params[:region]
-    @ladder_data = api.ladder_data(@region)
+    @region = params[:region].nil? ? "us" : params[:region]
+    @league = params[:region].nil? ? "grandmaster" : params[:league]
+    @ladder_data = api.ladder_data(@region, @league)
     @players = @ladder_data["team"].sort_by{|player| player["rating"]}.reverse
   end
 
--- lib/url_api.rb
+++ lib/url_api.rb
@@ -7,16 +7,31 @@ SlowWeb.limit('"https://kr.api.battle.net', 60, 1)
 class UrlApi
   @token = "gkxw63q8u6rspxhnt67hpywp"
   @key = "pcp7t55mgb3fznkw83qje72xjzsfnhnd"
-#  @API_URL2 = "https://#{region}.api.battle.net/sc2/ladder/#{ladder_id}?locale=#{lang}&apikey=#{key}"
   
-  def ladder_data(region)
-    url = "https://#{region}.api.battle.net/data/sc2/ladder/#{ladder_id(region)}?access_token=gkxw63q8u6rspxhnt67hpywp"
+  def ladder_data(region, league)
+    url = "https://#{region}.api.battle.net/data/sc2/ladder/#{ladder_id(region, league)}?access_token=gkxw63q8u6rspxhnt67hpywp"
     JSON.parse(HTTParty.get(url).body)
   end
 
-  def ladder_id(region)
-    league = "6"
-    gm_region = "https://#{region}.api.battle.net/data/sc2/league/29/201/0/#{league}?access_token=gkxw63q8u6rspxhnt67hpywp"
+  def ladder_id(region, league)
+    rank = 6;
+    case league
+    when "Grandmaster"
+      rank = 6
+    when "Masters"
+      rank = 5
+    when "Diamond"
+      rank = 4
+    when "Platinum"
+      rank = 3
+    when "Gold"
+      rank = 2
+    when "Silver"
+      rank = 1
+    when "Bronze"
+      rank = 0
+    end
+    gm_region = "https://#{region}.api.battle.net/data/sc2/league/29/201/0/#{rank}?access_token=gkxw63q8u6rspxhnt67hpywp"
     JSON.parse(HTTParty.get(gm_region).body)["tier"].first["division"].first["ladder_id"]
   end
 end
